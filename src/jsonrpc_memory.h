fa62be50b379f20734d7aa37a0fef959765507f3
Jonghyeok Lee <jhlee4bb@gmail.com>
2012-10-05 10:20:50 +0900

    add comments

--- src/jsonrpc_memory.h
+++ src/jsonrpc_memory.h
@@ -12,18 +12,39 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * same as 'malloc'
+ */
 void *  jsonrpc_malloc (size_t size);
 
+/**
+ * same as 'free'
+ */
 void    jsonrpc_free (void *mem);
 
+/**
+ * same as 'calloc'
+ */
 void *  jsonrpc_calloc (size_t count, size_t size);
 
+/**
+ * same as 'realloc'
+ */
 void *  jsonrpc_realloc (void *mem, size_t size);
 
+/**
+ * The function returns a pointer to a new memory which is a duplicated of the memory 'mem' with 'size'.
+ */
 void *	jsonrpc_memdup (const void *mem, size_t size);
 
+/**
+ * same as 'strdup'
+ */
 char *  jsonrpc_strdup (const char *str);
 
+/**
+ * example) jsonrpc_vfree(mem1, mem2, NULL);
+ */
 void    jsonrpc_vfree (void *mem, ...);
 
 #endif
--- src/jsonrpc_mstream.h
+++ src/jsonrpc_mstream.h
@@ -37,18 +37,44 @@ jsonrpc_mstream_close (jsonrpc_mstream_t *mstream);
  * @param fmt	format (same as printf's)
  * @param ap	variable-length argument
  * @return The number of characters that would have been writtend. (if error, return -1)
+ */
 int
 jsonrpc_mstream_vprint (jsonrpc_mstream_t *mstream, const char *fmt, va_list ap);
 
+/**
+ * Formatted input to memory stream
+ *
+ * @param mstream	instance of 'jsonrpc_mstream_t'
+ * @param fmt	format (same as printf's)
+ * @param ...	variable-length argument
+ * @return The number of characters that would have been written. (if error, return -1)
+ */
 int
 jsonrpc_mstream_print (jsonrpc_mstream_t *mstream, const char *fmt, ...);
 
+/**
+ * The number of written characters
+ *
+ * @param mstream	instance of 'jsonrpc_mstream_t'
+ * @return The number of written characters
+ */
 size_t
 jsonrpc_mstream_length (jsonrpc_mstream_t *mstream);
 
+/**
+ * Set the memory position indicator to the beginning of the stream.
+ *
+ * @param mstream	instance of 'jsonrpc_mstream_t'
+ */
 void
 jsonrpc_mstream_rewind (jsonrpc_mstream_t *mstream);
 
+/**
+ * Get memory stream buffer
+ *
+ * @param mstream	instance of 'jsonrpc_mstream_t'
+ * @return	'\0' terminated string
+ */
 const char *
 jsonrpc_mstream_getbuf (jsonrpc_mstream_t *mstream);
 
